84a1500a0e5414e2f27571db905979e5e5bc8f8f
Hunter McGregor <yatamare@hotmail.com>
2017-11-05 13:05:07 -0600

    Furthering of the pathfinder
    Updated pathfinder

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 // The enemies ask for a path.
 // Threat Types: 0 - wall; 1- fire ; 2 - poison; 3 - oil
-// Direction Types: 0 - north ; 2 - east; 3 - south; 4 - west
-
-// GetAdjecent (current, direction(0-4))
-// array of threats getThreat() size 4
-
-// NEEDS WORK: FIND SMALLEST
 public class PathFinder : MonoBehaviour {
 
     // Player Info
     private GameObject player;
     private Vector3 playerLocation;
 
+    // Threat Map access
+    private GameObject gameMaster;
+    private ThreatSCRIPT threatMap;
+
     // Path Lists
-    private List<Vector3> openList = new List<Vector3>();
-    private List<Vector3> closeList = new List<Vector3>();
+    private List<Path> openList = new List<Path>();
+    private List<Path> closedList = new List<Path>();
 
     // Unity Methods
     ////////////////
     void start(){
         // Find the player object
         player = GameObject.FindGameObjectWithTag("Player");
+        // Find the threat map
+        gameMaster = GameObject.FindGameObjectWithTag("GameMaster");
+        threatMap = gameMaster.GetComponent<>();
         // Look for the players location every second
         InvokeRepeating("findPlayer", 0f, 1f);
     }
@@ -34,44 +35,70 @@ public class PathFinder : MonoBehaviour {
     // Path Finding
     ////////////////
     public Vector3[] FindPath(Vector3 currentVector){
+        // Path hasn't been found
         bool pathFound = false;
-        Vector3 currentLocation = currentVector;
-        Vector3 smallestLocation;
 
-        Vector3 north;
-        Vector3 east;
-        Vector3 south;
-        Vector3 west;
+        // Set up the first path
+        Vector3 currentPosition = currentVector;
+        Path currentParent = null;
+        int currentScore = TileWeight(currentPosition);
+        Path currentPath = new Path(currentPosition, currentParent, currentScore);
 
-        // Add starting position to open list
-        openList.Add(currentLocation);
+        // Stores the Smallest Location
+        Path smallestLocation;
+
+        // Stores for the adjacent squares
+        // 0 - north; 1 - east; 2 - south; 3 - west
+        Vector3[] adjacentSquares = new Vector3[4];
+        Path adjacentPath;
+        int adjacentScore;
+
+        // Clear open and closed list
+        openList.Clear();
+        closedList.Clear()
+
+        // Add starting path to open list
+        openList.Add(currentPath);
         do {
             // Find the smallest vector/tile in open list
             smallestLocation = FindSmallest();
             // Make the smallest the current location
-            currentLocation = smallestLocation;
+            currentPath = smallestLocation;
             // Add current location to the closed list
-            closedList.Add(currentLocation);
+            closedList.Add(currentPath);
             // Remove current location from the open list
-            openList.Remove(currentLocation);
+            openList.Remove(currentPath);
+
             // Check to see if we arrived at destination
-            if ((currentLocation.x > playerLocation.x-0.5) && (currentLocation.x < playerLocation.x+0.5)){
-                if  ((currentLocation.y > playerLocation.y-0.5) && (currentLocation.y > playerLocation.y+0.5)) {
+            currentPosition = smallestLocation.GetPosition();
+            if ((currentPosition.x > playerLocation.x-0.5) && (currentPosition.x < playerLocation.x+0.5)){
+                if  ((currentPosition.y > playerLocation.y-0.5) && (currentPosition.y > playerLocation.y+0.5)) {
                     pathFound = true;
                     break;
                 }
             }
+
             // retreive all adjacent squares
-            north = new Vector3(currentLocation.x,currentLocation.y+0.5,0);
-            south = new Vector3(currentLocation.x,currentLocation.y-0.5,0);
-            east = new Vector3(currentLocation.x+0.5,currentLocation.y,0);
-            west = new Vector3(currentLocation.x-0.5,currentLocation.y,0);
+            adjacentSquares[0] = new Vector3(currentLocation.x,currentLocation.y+0.5,0);
+            adjacentSquares[1] = new Vector3(currentLocation.x,currentLocation.y-0.5,0);
+            adjacentSquares[2] = new Vector3(currentLocation.x+0.5,currentLocation.y,0);
+            adjacentSquares[3] = new Vector3(currentLocation.x-0.5,currentLocation.y,0);
 
-
-            // for loop through it adjacent square
-                // if it is already in the closed list, or cant go, ignore it
-                // if it not in the open list, compute the score, add it to open list
-                // else if its already in the open list, check to see if its a better path
+             // for loop through the adjacent square
+            for (int i = 0; i < 4; i + 1){
+                adjacentScore = TileWeight(adjacentSquare[i]);
+                adjacentPath = new Path(adjacentSquare[i],currentPath,adjacentScore);
+                // if it is already in closed list, ignore it
+                if (!(closedList.Contains(adjacentPath))){
+                    // if you cannot pass the square, ignore it
+                    if (adjacentPath.GetScore > 1000){
+                         // if its not in the open list, add it
+                        if (!(openList.Contains(adjacentPath))){
+                            openList.Add(adjacentPath);
+                        }
+                    }
+                }
+            }
          // Continue until there is no more available square in the open list (which means there is no path)
         } while (openList.Count > 0);
         if (pathFound == true){
@@ -82,15 +109,17 @@ public class PathFinder : MonoBehaviour {
         }
     }
 
-    private Vector3 FindSmallest(){
+    private Path FindSmallest(){
+        // Save the list length
         int listLength = openList.Count;
-        int smallestWeight;
-        Vector3 currentSmallest;
-
+        // Holder variables
+        Path currentSmallest;
+        // Assign the first value to smallest
         currentSmallest = openList.Item[0];
-
+        smallestWeight = currentSmallest.GetScore();
+        // Loop Through openList finding the smallest tile weight
         for (int i = 1; i < openList.Count; i += 1){
-            contenderWeight = TileWeight(openList.Item[i], TILE CALL);
+            contenderWeight = openList.Item[i].GetScore;
             if (smallestWeight > contenderWeight){
                 smallestWeight = contenderWeight;
                 currentSmallest = openList.Item[i];
@@ -101,9 +130,10 @@ public class PathFinder : MonoBehaviour {
 
     // Calculating Weights
     ////////////////
-    private int TileWeight(Vector3 currentVector, var Tile){
+    private int TileWeight(Vector3 currentVector){
+        int[] tile = threatMap.getInfluenceNode(currentVector).getThreat()
         int h = this.DistanceToPlayer(currentVector);
-        int f = this.ThreatWeight(Tile);
+        int f = this.ThreatWeight(tile);
         return f+h;
     }
 
@@ -113,7 +143,7 @@ public class PathFinder : MonoBehaviour {
         return (xDifference+yDifference);
     }
 
-    private int ThreatWeight(){
+    private int ThreatWeight(int[] tile){
 
     }
 
@@ -121,12 +151,5 @@ public class PathFinder : MonoBehaviour {
     ////////////////
     private void findPlayer(){
         playerLocation = player.transform.position;
-    }
-
-    // Nested class for the paths
-    ////////////////s
-    private class Paths{
-        public Vector3 location;
-        public Vector3 parent;
     }
 }
