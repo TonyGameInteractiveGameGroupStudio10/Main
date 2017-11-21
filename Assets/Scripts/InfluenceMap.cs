1f169c7879bdd1cdcce537e358e334aa86f7d254
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-11-21 13:44:36 -0600

    updated scaling for the influence map

--- Assets/Scripts/InfluenceMap.cs
+++ Assets/Scripts/InfluenceMap.cs
@@ -8,16 +8,14 @@ public class InfluenceMap : MonoBehaviour {
 	// each block is 0.5 world units long
 	private static int gridWidth = 40;
 	private static int gridLength = 40;
-	private static int scaledLength = gridLength * 2;
-	private static int scaledWidth = gridWidth * 2;
 
 	//y,x for efficiency I thenk 
-	public static InfluenceNode[,] influenceMap = new InfluenceNode[scaledLength, scaledWidth];
+	public static InfluenceNode[,] influenceMap = new InfluenceNode[gridLength, gridWidth];
 
 
 	public void Start(){
-		for(int i = 0; i < scaledLength; i++){
-			for(int j = 0; j < scaledWidth; j++){
+		for(int i = 0; i < gridLength; i++){
+			for(int j = 0; j < gridWidth; j++){
 				influenceMap[i,j] = new InfluenceNode(0);
 			}
 		}
@@ -45,31 +43,8 @@ public class InfluenceMap : MonoBehaviour {
 
 	// takes the world position and scales it to the nearest .5
 	public Vector2 scaleWorldPos(Vector3 worldPos){
-		
-		Vector2 conVec = new Vector2(worldPos.x, worldPos.y);
+		Vector2 conVec = new Vector2((float) Mathf.Round(worldPos.x), (float) Mathf.Round(worldPos.y));
 
-		int x = (int) worldPos.x;
-		int y = (int) worldPos.y;
-
-		if(conVec.x < x+0.3f){
-			conVec.x = (float) x;
-		}
-		else if (conVec.x > x+0.7f){
-			conVec.x = x + 1.0f;
-		}
-		else{
-			conVec.x = x + 0.5f;
-		}
-
-		if(conVec.y < y+0.3f){
-			conVec.y = (float) y;
-		}
-		else if(conVec.y > y+0.7f){
-			conVec.y = y + 1.0f;
-		}
-		else{
-			conVec.y = y + 0.5f;
-		}
 		return conVec;
 	}
 
@@ -77,7 +52,7 @@ public class InfluenceMap : MonoBehaviour {
 
 	public IntVector2 worldPosToGrid(Vector3 worldPos){
 		Vector2 conWorldPos = scaleWorldPos(worldPos);
-		return new IntVector2(((int)(conWorldPos.x+20)*2),((int)(conWorldPos.y+20)*2));
+		return new IntVector2(((int)(conWorldPos.x+20)),((int)(conWorldPos.y+20)));
 	}
 
 
@@ -107,10 +82,10 @@ public class InfluenceMap : MonoBehaviour {
 		}
 
 		for(int i = -3; i <= radius; i++) {
-			if(((center.y + i) < scaledLength) && ((center.y + i) > 0)) {
+			if(((center.y + i) < gridLength) && ((center.y + i) > 0)) {
 				influenceMap[center.y+i , center.x].getThreat()[type] += threatVal * (decayFactor * i);
 				for(spread = (radius - Mathf.Abs(i)); spread > 0; spread--){
-					if((center.x + spread) < scaledWidth) {
+					if((center.x + spread) < gridWidth) {
 						influenceMap[center.y+i , center.x+spread].getThreat()[type] += threatVal * (decayFactor * spread);
 					}
 					if((center.x - spread) > 0){
