62ea3f4e15568260fc567fffa7c402375e2b2158
Hunter McGregor <yatamare@hotmail.com>
2017-11-28 12:34:36 -0600

    Commenting
    commenting

--- Assets/Scripts/WeaponIce.cs
+++ Assets/Scripts/WeaponIce.cs
@@ -7,29 +7,25 @@ public class WeaponIce : MonoBehaviour {
     void Start(){
         Invoke("DestroySelf", 5f);
     }
-    
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player"){
             collision.gameObject.SendMessage("TakeDamage", 2);
             //StartCoroutine(DelayedFreeze());
             collision.gameObject.SendMessage("SetSpeed", 0.0f);
             StartCoroutine(ResetSpeed());
             collision.gameObject.SendMessage("SetSpeed", 4.0f);
         }
-        if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<MonsterClass>().getType() != 4)
-        {
+        if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<MonsterClass>().getType() != 4){
             collision.gameObject.SendMessage("TakeDamage", 2);
             collision.gameObject.SendMessage("ReceivingStun");       
         }
     }
-    IEnumerator DelayedFreeze()
-    {
+
+    IEnumerator DelayedFreeze(){
         yield return new WaitForSeconds(1.5f);
     }
-    IEnumerator ResetSpeed()
-    {
+    IEnumerator ResetSpeed(){
         yield return new WaitForSeconds(1.0f);
     }
 
