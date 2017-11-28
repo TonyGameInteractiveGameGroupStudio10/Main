e043dbbc226ba11f0aec52b6d202d94108dcf199
Hunter McGregor <yatamare@hotmail.com>
2017-11-28 13:51:59 -0600

    Update to play
    fixing to play

--- Assets/Scripts/WeaponFire.cs
+++ Assets/Scripts/WeaponFire.cs
@@ -7,36 +7,39 @@ public class WeaponFire : MonoBehaviour {
     public float speed = 5.0f;
     public Vector3 FiringDirection;
 
-	// Use this for initialization
+	
+    void Start(){
+        Invoke("DestroySelf", 5f);
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         this.GetComponent<Rigidbody2D>().velocity = FiringDirection.normalized * speed;
         Destroy(gameObject, 7);
 	}
-    public void setFiringDirection(Vector3 Direction)
-    {
+
+    public void setFiringDirection(Vector3 Direction){
         FiringDirection = Direction;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag =="Player")
-        {
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag =="Player"){
             collision.gameObject.SendMessage("TakeDamage", 5);
             Destroy(gameObject);
         }
-        if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<MonsterClass>().getType() != 1)
-        {
+        if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<MonsterClass>().getType() != 1){
             collision.gameObject.SendMessage("TakeDamage", 5);
             Destroy(gameObject);
         }
-        if (collision.gameObject.tag == "Wall")
-        {
+        if (collision.gameObject.tag == "Wall"){
             Destroy(gameObject);
         }
-        if(collision.gameObject.tag == "DropWall")
-        {
+        if(collision.gameObject.tag == "DropWall"){
             Destroy(gameObject);
         }
+    }
+
+    private void DestroySelf(){
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/WeaponIce.cs
+++ Assets/Scripts/WeaponIce.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponIce : MonoBehaviour {
+    // Time Player has before freeze
+    private float playerTimer;
+    // Reference to player object
+    private GameObject playerRef;
+    // Is the player inside the freeze
+    private bool playerInside;
 
+    ///////////////////////////////////
+    // Unity Methods
+    ///////////////////////////////////
     void Start(){
+        playerTimer = 1f;
+        playerInside = false;
         Invoke("DestroySelf", 3.5f);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
+    void Update(){
+      if (playerInside == true){
+        if (playerTimer > 0){
+          playerTimer -= Time.deltaTime;
+        } else {
+          playerRef.SendMessage("StunPlayer");
+          playerTimer = 1f;
+        }
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.tag == "Player"){
             collision.gameObject.SendMessage("TakeDamage", 2);
-            StartCoroutine(DelayedFreeze());
-            collision.gameObject.SendMessage("StunPlayer");   
+            playerInside = true;
+            playerRef = collision.gameObject;
         }
         if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<MonsterClass>().getType() != 4){
             collision.gameObject.SendMessage("TakeDamage", 2);
@@ -20,11 +42,16 @@ public class WeaponIce : MonoBehaviour {
         }
     }
 
-    IEnumerator DelayedFreeze(){
-        yield return new WaitForSeconds(1.5f);
+    void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player"){
+            playerInside = false;
+            playerTimer = 1f;
+        }
     }
-   
 
+    ///////////////////////////////////
+    // Methods
+    ///////////////////////////////////
     private void DestroySelf(){
       Destroy(gameObject);
     }
