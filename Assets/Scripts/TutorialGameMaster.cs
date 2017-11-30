17ecd8de224f0a2a3d8bcce01108400ffa306026
Hunter McGregor <yatamare@hotmail.com>
2017-11-29 21:00:59 -0600

    Fix to Spawn protectors
    Fixed spawn protectors and start tutorial level

---
+++ Assets/Scripts/TutorialGameMaster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialGameMaster : MonoBehaviour {
+
+	// Sprite Containers
+	public Sprite[] potionSprites = new Sprite[3];
+	public Sprite[] weaponSprites = new Sprite[1];
+	public Sprite[] attackSprites = new Sprite[5];
+
+	// Event Stuff
+	private float eventTimer;
+	private int eventPlace;
+
+	void Start(){
+
+	}
+
+	void Update(){
+
+	}
+}
