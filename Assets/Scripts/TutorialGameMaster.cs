b8552bc0afcecfc42ca032fdef817b2f9b83d45c
Hunter McGregor <yatamare@hotmail.com>
2017-12-01 16:02:32 -0600

    Tutorial Scene Update
    Updating the tutorial, more of a back up then really including anything just cause everything doesnt work yeeet

--- Assets/Scripts/TutorialGameMaster.cs
+++ Assets/Scripts/TutorialGameMaster.cs
@@ -14,14 +14,76 @@ public class TutorialGameMaster : GameMaster {
 	public GameObject shooting;
 	public GameObject powerUps;
 	public GameObject potions;
-	public GameObject monsters;
+	public GameObject monstersText;
+
+	// Props
+	public GameObject attackMod;
+	public GameObject potionHealth;
+	public GameObject potionHaste;
+	public GameObject potionClear;
 
 
 	void Start(){
-
+		welcome.SetActive(false);
+		movement.SetActive(false);
+		shooting.SetActive(false);
+		powerUps.SetActive(false);
+		potions.SetActive(false);
+		monstersText.SetActive(false);
+		attackMod.SetActive(false);
+		potionHealth.SetActive(false);
+		potionHaste.SetActive(false);
+		potionClear.SetActive(false);
+		this.WelcomePhase();
+		eventTimer = 8f;
+		eventPlace = 1;
 	}
 
 	void Update(){
+		if (eventTimer > 0){
+			eventTimer -= Time.deltaTime;
+		}
+		else if(eventPlace == 1){
+
+		}
+		else if(eventPlace == 2){
+
+		}
+		else if(eventPlace == 3){
+
+		}
+		else if(eventPlace == 4){
+
+		}
+		else if(eventPlace == 5){
+
+		}
+		else{
+
+		}
+	}
+
+	public void WelcomePhase(){
+
+	}
+
+	public void MovementPhase(){
+
+	}
+
+	public void ShootingPhase(){
+
+	}
+
+	public void PowerUpPhase(){
+
+	}
+
+	public void PotionPhase(){
+
+	}
+
+	public void MonsterPhase(){
 
 	}
 }
